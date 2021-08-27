13c067a57aa56006958d57ca811ed55df9487cb1
Srdjan Grbic <69905261+Srki04@users.noreply.github.com>
2021-08-28 00:30:28 +0200

    LogIn/Registration Mechanics Almost Done!
    LogIn/Registration Mechanics is still in progress, but it is near its end.
    I am commiting and pushing this now, since I will take a short nap break, since it is 1 AM now and will return at 7 AM to finish this Mechanics.

--- Code/PHP/joining.php
+++ Code/PHP/joining.php
@@ -1,3 +1,10 @@
+<?php
+
+session_start(); 
+include "/SharpShark/Code/PHP/connection.php"; 
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -15,18 +22,38 @@
             <img class="BackgroundImage" src="/SharpShark/Images/RegistrationBackground.png">
 	        <form method = "POST" action="#">
 			        <div class = "JoiningInformation">
-                        <input name = "Username" type = "text" placeholder = "Username"> <br> <br>
-                        <input name = "Mail" type = "text" placeholder = "E-Mail">
-                        <input name = "Password" type = "password" placeholder = "Password">
-                        <input name = "RepeatedPassword" type = "text" placeholder = "RepeatPassword">
-                        <input name = "DatePfBirth" type = "date" placeholder = "Date of Birth">
+                        <input name = "Username" type = "text" placeholder = "Username" required> <br> <br>
+                        <input name = "Mail" type = "text" placeholder = "E-Mail" required>
+                        <input name = "Password" type = "password" placeholder = "Password" required>
+                        <input name = "RepeatedPassword" type = "text" placeholder = "RepeatPassword" required>
+                        <input name = "DatePfBirth" type = "date" placeholder = "Date of Birth" required>
                         <div class = "JoinButton">
                             <button type = "submit" name = "JoinButton">Dive In</button>
                             <button name = "BackToHome">Float Out</button>
                         </div>
                     </div>
                     <?php
-                        if(isset($_POST["BackToHome"])) header("Location: /SharpShark/index.php"); 
+                        if(isset($_POST["JoinButton"])){ // Checking if the User has pressed the button for creating the account
+
+                            // Collecting Users Inputed Information
+                            $Username = $_POST["username"]; 
+                            $EMailAdress = $_POST["Mail"]; 
+                            $Password = $_POST["password"]; 
+                            $RepeatedPassword = $_POST["RepeatedPassword"]; 
+                            $DateOfBirth = $_POST["Birthday"];
+                            
+                            // Checking if the User is Eligible for Creating an Account
+                            // checking if the passwords are matching.. 
+                            if(strcmp($Password, $RepeatedPassword) != 0) echo "<h4 style = 'color: #83CDBB'> Repeated Password does not Match with First Password :~ ! </h4>" 
+                            else if (False) echo "Something :D :D . "; // TODO:: Add Terms of Service and Check is User Old Enough
+                            else{ // Now see if there already is the User with the Same Username
+                                $DoesUserAlreadyExistQuery = "SELECT * FROM users WHERE username = '$Username' "; 
+                                $SameUserQueryResult = mysqli_query($Link, $DoesUserAlreadyExistQuery) or die ("There was an Error. We are so so sorry, please please try again Later On ;) :) . "); 
+                                // TODO:: Continue Building this Registration Mechanics until the End
+                            }
+
+                        }
+                        else if(isset($_POST["BackToHome"])) header("Location: /SharpShark/index.php"); 
                     ?>
             </form>
         </main>
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+
+session_start(); 
+include "Code/PHP/connection.php"; 
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -27,7 +34,7 @@
            		   </div>
            		   <div class="div">
            		   		<h5>Username</h5>
-           		   		<input type="text" class="input">
+           		   		<input name = "UsernameInputField" type="text" class="input">
            		   </div>
            		</div>
            		<div class="input-div pass">
@@ -36,14 +43,53 @@
            		   </div>
            		   <div class="div">
            		    	<h5>Password</h5>
-           		    	<input type="password" class="input">
+           		    	<input name = "PasswordInputField" type="password" class="input">
             	   </div>
             	</div>
             	<a href="#">Forgot Password?</a>
             	<button name = "PlayButton" type = "submit" class = "btn">Play</button>
                 <button name = "JoinButton" class = "btn">Join the Network</button>
 				<?php
-					if(isset($_POST["JoinButton"])) header("Location: Code/PHP/joining.php"); 
+					if(isset($_POST["PlayButton"])){ // Checking if the LogIn Button is Pressed
+
+						// Getting Users Input
+						$Username = $_POST["UsernameInputField"]; 
+						$Password = $_POST["PasswordInputField"]; 
+
+						// Making Query to Find that User in the Database
+						$Query = "SELECT * FROM users WHERE username = '$Username' and password = '$Password' "; 
+						$QueryResult = mysqli_query($Link, $Query) or die ("There was an Error :( . So Sorry. Please try again later on :) . "); 
+						$DatabaseRows = $QueryResult -> num_rows; 
+						if($DatabaseRows > 0){
+							// User Founded! 
+							
+							// Storing all of the Users Information from Database into a variables
+							$UserID = $DatabaseRows[0]; 
+							$IsUserAdmin = $DatabaseRows[1]; 
+							$EMailAdress = $DatabaseRows[3];  
+							$DateOfBirth = $DatabaseRows[5]; 
+
+							// Storing all of the Users Information from Variables to Sessions
+							$_SESSION["id"] = $UserID;
+							$_SESSION["admin"] = $IsUserAdmin; 
+							$_SESSION["username"] = $Username; 
+							$_SESSION["email"] = $EMailAdress; 
+							$_SESSION["password"] = $Password; 
+							$_SESSION["birthday"] = $DateOfBirth; 
+
+							header("Location: Code/PHP/profile.php"); // Transporting User to its Profile
+							
+
+						}
+						else { // Username does not Match with Password or maybe does not exist in the Database
+							// Checking if the user even exist in the Database and telling user that user does not exist, if user does not exist or that user has entered just wrong password
+							$DoesUserExistQuery = "SELECT * FROM users WHERE username = '$Username' "; 
+							if(mysqli_query($Link, $DoesUserExistQuery) -> num_rows > 0) echo "<h4 style = 'color: #83CDBB'> You Entered Wrong Password for that User! </h4>";
+							else echo "<h4 style = 'color: #83CDBB'> That user does not exist (Try to Enter different Username) ! </h4>";  
+						}
+
+					}
+					else if(isset($_POST["JoinButton"])) header("Location: Code/PHP/joining.php"); // If LogIn is not pressed, checking if the button for creating a new ac is pressed
 				?>
             </form>
         </div>
